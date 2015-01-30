06858110dc7f6f0c16605e87daaf5ff346261547
smist976 <stephanie.smithson@plantandfood.co.nz>
2015-01-30 13:58:36 +1300

    update

---
+++ RainShelter Barley/Soil Water Deficit App/dbconnections.R
@@ -0,0 +1,13 @@
+# connect to database
+DBConnection <- function(){
+  library (RPostgreSQL)
+  drv <- dbDriver('PostgreSQL')
+  creds <- scan('C:/GitHubRepos/Summer Project - R/RainShelter Barley/RainShelterdbLogin.txt',what='character',sep=",")
+  con <- dbConnect(drv, dbname='rainshelter', host='aklpdb22.pfr.co.nz', port='5432', user=creds[[1]], password=creds[[2]])
+  return (con)
+}
+
+#close db connection
+closeDBConnection <- function (con) {
+  dbDisconnect(con)
+}
